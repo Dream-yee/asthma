8b0796e6b9a8d79cd61dace0561d0f302d9816ab
Dreamyee <morganwen0126@gmail.com>
2026-03-01 22:54:13 +0900

    去學測組別

--- python/tools/match_groups.py
+++ python/tools/match_groups.py
@@ -105,8 +105,11 @@ def process_and_match_data(
             
             # 2. 查找匹配的組別代號
             group_id = subject_map.get(required_subjects)
+            remove_gsat_group_id = subject_map.get(frozenset(filter(lambda x: not x in ["國文", "英文", "數A", "數B", "自然", "社會"], multipliers.keys())))
             
             dept_data["組別代號"] = group_id
+            dept_data["去學測組別代號"] = remove_gsat_group_id
+
             dept_data["達標比例"] = None # 預設為 None
 
             if group_id:
